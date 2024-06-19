db55de74d22dc6f589ad14235fa57928dbf10214
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2024-06-18 18:20:10 -0700

    Update SQLQuery_1.sql

--- Day1_SQL/SQLQuery_1.sql
+++ Day1_SQL/SQLQuery_1.sql
@@ -15,28 +15,28 @@ WHERE ListPrice != 0
 --3. Write a query that retrieves the columns ProductID, Name, Color and ListPrice from the Production.Product table, 
 --the rows that are NULL for the Color column.
 
-SELECT p.ProductID, p.Name, p.Color, p.ListPrice 
-FROM Production.Product p
-WHERE p.Color IS NULL
+SELECT ProductID, Name, Color, ListPrice 
+FROM Production.Product
+WHERE Color IS NULL
 
 --4.Write a query that retrieves the columns ProductID, Name, Color and ListPrice from the Production.Product table, 
 --the rows that are not NULL for the Color column.
-SELECT p.ProductID, p.Name, p.Color, p.ListPrice 
-FROM Production.Product p
-WHERE p.Color IS NOT NULL
+SELECT ProductID, Name, Color, ListPrice 
+FROM Production.Product
+WHERE Color IS NOT NULL
 
 --5.Write a query that retrieves the columns ProductID, Name, Color and ListPrice from the Production.Product table, 
 --the rows that are not NULL for the column Color, and the column ListPrice has a value greater than zero.
 
-SELECT p.ProductID, p.Name, p.Color, p.ListPrice 
-FROM Production.Product p
-WHERE p.Color IS NOT NULL AND p.ListPrice> 0
+SELECT ProductID, Name, Color, ListPrice 
+FROM Production.Product
+WHERE Color IS NOT NULL AND ListPrice> 0
 
 --6.Write a query that concatenates the columns Name and Color from the Production.Product table 
 --by excluding the rows that are null for color.
-SELECT p.Name +' '+ p.Color
-FROM Production.Product p
-WHERE p.Color IS NOT NULL
+SELECT Name +' '+ Color
+FROM Production.Product
+WHERE Color IS NOT NULL
 
 --7.Write a query that generates the following result set  from Production.Product:
 --NAME: LL Crankarm  --  COLOR: Black
@@ -45,21 +45,22 @@ WHERE p.Color IS NOT NULL
 --NAME: Chainring Bolts  --  COLOR: Silver
 --NAME: Chainring Nut  --  COLOR: Silver
 --NAME: Chainring  --  COLOR: Black
-SELECT  'NAME:'+p.Name + ' -- COLOR:'+ p.Color
-FROM Production.Product p
-WHERE p.Color IS NOT NULL
+  
+SELECT  'NAME:'+ Name + ' -- COLOR:'+ Color
+FROM Production.Product
+WHERE Color IS NOT NULL
 
 --8.Write a query to retrieve the to the columns ProductID and Name from the Production.Product table 
 --filtered by ProductID from 400 to 500
-SELECT p.ProductID, p.Name
-FROM Production.Product p
-WHERE p.ProductID BETWEEN 400 AND 500
+SELECT ProductID, Name
+FROM Production.Product
+WHERE ProductID BETWEEN 400 AND 500
 
 --9.Write a query to retrieve the to the columns  ProductID, Name and color from the Production.Product table 
 --restricted to the colors black and blue
 SELECT ProductID, Name, Color
 FROM Production.Product
-WHERE Color IN('black','blue')
+WHERE Color IN('black', 'blue')
 
 --10.Write a query to get a result set on products that begins with the letter S. 
 
