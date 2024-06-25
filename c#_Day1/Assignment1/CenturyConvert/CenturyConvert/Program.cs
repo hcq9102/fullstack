5391b225a525b85b7e357e087cb039b197d7f2e1
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2024-06-25 16:41:40 -0700

    Add files via upload

---
+++ c#_Day1/Assignment1/CenturyConvert/CenturyConvert/Program.cs
@@ -0,0 +1,18 @@
+﻿ // See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter number of centuries: ");
+int centuries = int.Parse(Console.ReadLine());
+
+ // transformation
+
+ int years = centuries * 100;
+ long days = (long)(years * 365.2425); // leap years over the centuries
+ long hours = days * 24;
+ long minutes = hours * 60;
+ long seconds = minutes * 60;
+ long milliseconds = seconds * 1000;
+ decimal microseconds = milliseconds * 1000m;
+ decimal nanoseconds = microseconds * 1000m;
+
+// print results
+ Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes = {seconds} seconds = {milliseconds} milliseconds = {microseconds} microseconds = {nanoseconds} nanoseconds");
