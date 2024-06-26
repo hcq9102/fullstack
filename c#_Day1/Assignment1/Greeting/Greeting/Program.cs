506e4022ec4cce9b955c9196beb10939464ae6c6
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2024-06-25 17:46:17 -0700

    Add files via upload

---
+++ c#_Day1/Assignment1/Greeting/Greeting/Program.cs
@@ -0,0 +1,21 @@
+﻿// Get the current time
+DateTime currentTime = DateTime.Now;
+int currentHour = currentTime.Hour;
+
+// provide appropriate greeting based on the current time(hour)
+if (currentHour >= 5 && currentHour < 12)
+{
+    Console.WriteLine("Good Morning");
+}
+else if (currentHour >= 12 && currentHour < 17)
+{
+    Console.WriteLine("Good Afternoon");
+}
+else if (currentHour >= 17 && currentHour < 21)
+{
+    Console.WriteLine("Good Evening");
+}
+else if ((currentHour >= 21 && currentHour <= 23) || (currentHour >= 0 && currentHour < 5))
+{
+    Console.WriteLine("Good Night");
+}
