d103aca257049fa3086f41ae48b08817a20d35fd
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2024-06-25 17:54:32 -0700

    Add files via upload

---
+++ c#_Day1/Assignment2/RotateArraySum/RotateArraySum/Program.cs
@@ -0,0 +1,29 @@
+﻿// enter input array and k value from console
+int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int k = int.Parse(Console.ReadLine());
+
+int n = input.Length;
+int[] sum = new int[n];
+
+// start rotation
+for (int r = 1; r <= k; r++)
+{
+    int[] rotated = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        rotated[(i + r) % n] = input[i];
+
+    }
+
+    // get sum
+    for (int j = 0; j < n; j++)
+    {
+        sum[j] += rotated[j];
+    }
+
+    // result show
+    Console.WriteLine(string.Join(" ", sum));
+}
+
+
