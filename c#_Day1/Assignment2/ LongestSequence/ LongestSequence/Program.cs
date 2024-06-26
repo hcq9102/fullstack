ce989868517f6b6586b6c58b32e978c37af129f0
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2024-06-25 17:55:32 -0700

    Add files via upload

---
+++ c#_Day1/Assignment2/ LongestSequence/ LongestSequence/Program.cs
@@ -0,0 +1,37 @@
+﻿// Read array from the console
+int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+// Variables to keep track of the longest sequence
+int maxLen = 0;
+int bestStart = 0;
+
+int left = 0;
+
+while (left < input.Length)
+{
+    int right = left;
+
+    // Move right pointer to the end of the current sequence of equal elements
+    while (right < input.Length && input[right] == input[left])
+    {
+        right++;
+    }
+
+    // Update maxLength and bestStart if a longer sequence is found
+    if (right - left > maxLen)
+    {
+        maxLen = right - left;
+        bestStart = left;
+    }
+
+
+    // Move left pointer to the start of the next sequence
+    left = right;
+}
+
+// Print the longest sequence
+Console.WriteLine("The longest sequence is: ");
+for (int i = 0; i < maxLen; i++)
+{
+    Console.Write(input[bestStart] + " ");
+}
