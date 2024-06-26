3d2b3c535628c71e923fa720236550576a4315a5
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2024-06-25 17:50:08 -0700

    Add files via upload

---
+++ c#_Day1/Assignment2/CopyArray/CopyArray/Program.cs
@@ -0,0 +1,28 @@
+﻿// Initialize the original array with 10 items
+int[] originalArray = new int[10] { 0,1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+// Create new array with the same length as the original array
+int[] copiedArray = new int[originalArray.Length];
+
+// copy values from the original array to the new array
+for (int i = 0; i < originalArray.Length; i++)
+{
+    copiedArray[i] = originalArray[i];
+}
+
+// Print the original array
+Console.WriteLine("Original Array:");
+foreach (int item in originalArray)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+
+// Print the copied array
+Console.WriteLine("Copied Array:");
+foreach (int item in copiedArray)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+
