0e3da03914e70fcf39a43076efd9e01c3daaaf18
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2024-06-25 17:46:55 -0700

    Add files via upload

---
+++ c#_Day1/Assignment1/Increment/Increment/Program.cs
@@ -0,0 +1,15 @@
+﻿//increments from 1 to 4
+for (int increment = 1; increment <= 4; increment++)
+{
+    // Inner loop: counts from 0 to 24, increasing by the value of the outer loop's control variable increment
+    for (int i = 0; i <= 24; i += increment)
+    {
+        Console.Write(i);
+        if (i + increment <= 24)
+        {
+            Console.Write(","); // Add a comma
+        }
+    }
+    Console.WriteLine(); // new line
+}
+
