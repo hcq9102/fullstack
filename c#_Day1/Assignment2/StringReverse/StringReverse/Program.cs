180f0f4326cde15a03415feb2110da1fea0a5670
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2024-06-25 17:59:29 -0700

    Add files via upload

---
+++ c#_Day1/Assignment2/StringReverse/StringReverse/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//----- method 1------
+//Convert the string to char array, reverse it, then convert it to string again
+Console.WriteLine("Enter a string:");
+string input = Console.ReadLine();
+
+char[] charArray = input.ToCharArray();
+
+Array.Reverse(charArray);
+
+string reversedString = new string(charArray);
+Console.WriteLine("Reversed string (method 1): " + reversedString);
+
+
+
+// ------ method2---------
+//Print the letters of the string in back direction (from the last to the first) in a for-loop
+Console.WriteLine("Enter a string:");
+string input2 = Console.ReadLine();
+
+// for loop
+Console.Write("Reversed string (method 2): ");
+for (int i = input2.Length - 1; i >= 0; i--)
+{
+    Console.Write(input2[i]);
+}
+Console.WriteLine();
