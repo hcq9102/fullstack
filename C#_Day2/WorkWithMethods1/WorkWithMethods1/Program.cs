a95d6011308551225bf8fd8d287354b6b0d66ac5
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2024-06-27 01:18:53 -0700

    upload remaining

---
+++ C#_Day2/WorkWithMethods1/WorkWithMethods1/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+// Define a class named ReverseArray
+class ReverseArray
+{
+    // Define  GenerateNumbers method that take desired length of array
+    public static int[] GenerateNumbers(int len)
+    {
+        Random random = new Random();
+        int[] numbers = new int[len];
+
+        for (int i = 0; i < len; i++)
+        {
+            numbers[i] = random.Next(1, 101); // Generate random numbers between 1 and 100
+        }
+
+        return numbers;
+    }
+
+    // Define Reverse method
+    public static void Reverse(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n/2; i++)
+        {
+            int temp = arr[i];
+            arr[i] = arr[n - i - 1];
+            arr[n - i - 1] = temp;
+        }
+    }
+
+    public static void PrintNumbers(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+
+    // Main method
+    public static void Main()
+    {
+        int n = 10;
+        int[] numbers = GenerateNumbers(n);
+        Console.Write("origin array: \n");
+        PrintNumbers(numbers);
+
+        Reverse(numbers);
+
+        Console.Write("Reversed array: \n");
+        PrintNumbers(numbers);
+
+    }
+}
---
+++ C#_Day2/WorkWithMethods2/WorkWithMethods2/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class FibonacciSequence
+{
+    // Recursive method to calculate the nth Fibonacci number
+    static int Fibonacci(int n)
+    {
+        // Base cases
+        if (n == 1 || n == 2)
+        {
+            return 1;
+        }
+        // Recursive case
+        else
+        {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+
+    static void Main()
+    {
+        // Print the first 10 numbers of the Fibonacci sequence
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"Fibonacci({i}) is : {Fibonacci(i)}");
+        }
+    }
+}
